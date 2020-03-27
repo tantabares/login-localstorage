fb61517269eb18a4615cb55b44347b41c7656786
Jhonathan Tabares <jhonathan.tabares@somoglobal.com>
2020-03-26 21:17:59 -0500

    Un mensajito bien descriptivo

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function logIn(param) {
+    let user = document.getElementById('username').value;
+    let pass = document.getElementById('password').value;
+
+    console.log(user + ' ' + pass);
+}
