fd55b3e765bfb333140306240abb423c0790a15d
yilmer <yilmer0001@hotmail.com>
2020-03-26 21:34:01 -0500

    Validación login

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
+const USR= "User";
+const PSW= "acamica"
 function logIn(param) {
     let user = document.getElementById('username').value;
     let pass = document.getElementById('password').value;
+
+    if((user===USR)&&(pass===PSW)){
+        const infoUser = {
+            user,
+            pass,
+        }
+        localStorage.setItem("Info",JSON.stringify(infoUser));
+        window.location.href= "/login-localstorage/home.html";
+    }else {
+        alert("La combinación de usuario y contraseña no son válidos");
+    }
 
     console.log(user + ' ' + pass);
 }
